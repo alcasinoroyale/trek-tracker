e5a6b5f0d36db667a51d5c134ac9ee1a61b818a9
github username <github email address>
2018-09-05 13:47:19 -0400

    edited signup link to reflect route changes. edited hiker resource to be plural.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#welcome'
 
-  resources :hiker, only: [:new, :create, :show]
+  resources :hikers, only: [:new, :create, :show]
 
   # get '/signup', to: 'hiker#new', as: 'new_hiker'
   # post '/signup', to: 'hiker#create'
