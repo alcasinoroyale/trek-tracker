86ccb75863287cab0ccee937b4520d223ca3e33e
github username <github email address>
2018-09-06 11:38:30 -0400

    basic authentication logic added to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #authenticate
+    @hiker = Hiker.find_by(username: params[:username])
+    if !!@hiker.authenticate(params[:password])
+      session[:hiker_id] = @hiker.id
+      raise @hiker.inspect
+      redirect_to hiker_path(@hiker)
+    else
+      redirect_to login_path
+    end
   end
+
 end
