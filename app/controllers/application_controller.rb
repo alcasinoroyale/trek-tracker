800aaf532cfed1e7e85bf1fa8916d983e2062608
github username <github email address>
2018-09-06 13:24:32 -0400

    wrote application helpers to require log in for certain pages. allowed some controller actions to skip that requirement.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+  skip_before_action :require_login, only: [:welcome]
+
+
 
   def welcome
 
   end
 
+  private
+
   def logged_in?
     !!session[:hiker_id]
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:message] = "You must be logged in to access this page"
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -1,4 +1,5 @@
 class HikersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
   #Sign Up Page
   def new
     @hiker = Hiker.new
