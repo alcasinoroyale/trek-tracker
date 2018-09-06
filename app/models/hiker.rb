f4d232dc81de802d06a507e5c754ad8cf2ab06fb
github username <github email address>
2018-09-06 14:49:06 -0400

    added validations to hiker and trail models. updated spec with progress.

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -3,4 +3,7 @@ class Hiker < ApplicationRecord
   has_many :trails, through: :aspirations
 
   has_secure_password
+
+  validates :username, :password_digest, presence: true
+  validates :username, uniqueness: true
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,8 @@
 class Trail < ApplicationRecord
   has_many :aspirations
   has_many :hikers, through: :aspirations
+
+  validates :name, :location, presence: true
+  validates :name, uniqueness: true
+
 end
