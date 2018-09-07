a815158e91eff713280a966e2c7a52f1e1482b01
github username <github email address>
2018-09-07 19:15:08 -0400

    created nested resource for hikers' aspirations.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#welcome'
 
-  resources :hikers, only: [:new, :create, :show]
+  resources :hikers, only: [:new, :create, :show] do
+    resources :aspirations, only: [:index, :new, :create]
+  end
+  
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
