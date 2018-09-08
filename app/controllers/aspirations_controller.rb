d2a28d406db2138549df73f6e4a9120a913ab0fb
github username <github email address>
2018-09-08 15:13:23 -0400

    added hometown info to hiker profile. created a bulleted list of trails in the hiker's aspirations path.

--- app/controllers/aspirations_controller.rb
+++ app/controllers/aspirations_controller.rb
@@ -1,7 +1,7 @@
 class AspirationsController < ApplicationController
   def index
     # params[:hiker_id] is the Hiker.id
-
+    @aspirations = Hiker.find_by(id: params[:hiker_id]).trails
   end
 
   def new
@@ -10,6 +10,6 @@ class AspirationsController < ApplicationController
   end
 
   def create
-
+    # logic to add a trail to this hiker's aspirations
   end
 end
