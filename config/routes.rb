7db1e8f9e77a81e81f4f5313ba6113b7a475992c
github username <github email address>
2018-09-10 12:05:47 -0400

    cleaned out comments in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   resources :hikers, only: [:new, :create, :show] do
     resources :aspirations, only: [:index, :new, :create]
   end
-  
+
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
-  # get '/signup', to: 'hiker#new', as: 'new_hiker'
-  # post '/signup', to: 'hiker#create'
+
+
 end
