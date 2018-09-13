7ab0a0ed5ccb80db74048a0af6771ec72cbefe94
github username <github email address>
2018-09-12 20:49:21 -0400

    added logic to controller to handle google auth. added notes to address hiker validation issue upon google login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @hiker = Hiker.find_by(username: params[:username]).try(:authenticate, params[:password])
-    
-    if !!@hiker
-      session[:hiker_id] = @hiker.id
+    if !!auth
+      @hiker = Hiker.find_or_create_by(username: auth.name)
+      # User is not valid without a HOMETOWN so I can't create just from Google.
+      # Two ideas: Redirect to sign in. OR I can just allow a user to validate
+      # WITHOUT a hometown. Then have an "Update Hometown" link LATER on the
+      # hiker#show page.
+      raise @hiker.inspect
+      @hiker.save
       redirect_to hiker_path(@hiker)
     else
-      flash[:message] = "Enter correct username and password, or sign up"
-      render 'sessions/new'
+      @hiker = Hiker.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+      if !!@hiker
+        session[:hiker_id] = @hiker.id
+        redirect_to hiker_path(@hiker)
+      else
+        flash[:message] = "Enter correct username and password, or sign up"
+        render 'sessions/new'
+      end
     end
   end
 
@@ -22,4 +33,9 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  private
+
+  def auth
+    request.env['omniauth.auth'].info
+  end
 end
