b71567615460152e525791474cdca6d83da1c9f5
Brittany <britt.cumens@gmail.com>
2018-09-27 12:31:40 -0400

    rails assessment live code finished. generated list from new scope method, information now renders in the view.

--- app/models/aspiration.rb
+++ app/models/aspiration.rb
@@ -3,7 +3,7 @@ class Aspiration < ApplicationRecord
   belongs_to :hiker
   accepts_nested_attributes_for :trail, reject_if: :reject_trails
 
-  scope :completed_aspirations, -> { joins(:hiker).where(:completed => "true").group(:username) }
+  scope :completed_aspirations, -> { joins(:hiker).where(:completed => "true") }
 
   validate :unique_trail, :on => :create
 
